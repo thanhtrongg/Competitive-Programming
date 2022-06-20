85ab82930d8078added50b404a6ec52ce63a05de
Phthonus <tt.trong.2108@gmail.com>
2022-06-20 22:08:05 +0700

    update

---
+++ Programming Techniques/Language Features/Shortening Code/sc.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+//Type Names
+typedef long long ll //long long a = 123456789; = ll a = 123456789;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+//Macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+/*
+v.push_back(make_pair(y1, x1));
+v.push_back(make_pair(y2, x2));
+int d = v[i].first + v[i].second;
+=
+v.PB(MP(y1, x1));
+v.PB(MP(y2, x2));
+int d = v[i].F + v[i].S;
+*/
+#define REP(i, a, b) for (int i = a; i <= b; i++);
+
+for (int i = 1; i <= n; i++) {
+
+}
+=
+REP (i, 1, n) {
+    
+}
+
+using namespace std;
+
+void bost() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main() {
+    bost();
+
+}
