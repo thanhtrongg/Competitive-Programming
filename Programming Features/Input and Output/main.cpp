394964528483c0ea4934e04026ae775595356e68
Phthonus <tt.trong.2108@gmail.com>
2022-06-20 12:14:23 +0700

    Create main.cpp

---
+++ Programming Features/Input and Output/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b;
+string x;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> a >> b >> x;
+
+    string s;
+    getline(cin, s);
+}
